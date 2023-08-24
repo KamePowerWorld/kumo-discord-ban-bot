e1dc7721ce0e3a207e888a42c02f7e423476e741
kumo0621 <kumokumokumo0621@gmail.com>
2023-08-25 00:10:45 +0900

    aaaaaa

--- BAN_BOT.py
+++ BAN_BOT.py
@@ -2,63 +2,80 @@ import os
 import discord
 from discord.ext import commands
 import datetime
+import yaml
+from discord import Embed
 
 intents = discord.Intents.default()
 intents.bans = True  # BANイベントを有効化
+intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+def load_config():
+    with open('ban_config.yml', 'r') as config_file:
+        return yaml.load(config_file, Loader=yaml.SafeLoader)
+    
 @bot.event
 async def on_ready():
+    global target_channel_id
+    global guild_id
     print(f'Logged in as {bot.user.name}')
     
-    # 起動時にメッセージを送信するチャンネルIDを設定
-    startup_channel_id = 1126060676989849712  # ここにチャンネルIDを入力
-    
-    startup_channel = bot.get_channel(startup_channel_id)
-    if startup_channel:
-        await startup_channel.send('くもぱわ～BANBOTが起動しました。')
+
+    config = load_config()
+    guild_id = config.get('guild_id', None)
+    target_channel_id = config.get('target_id', None)
+    if guild_id:
+        guild = bot.get_guild(guild_id)
+        
+        if guild:
+            await guild.chunk()
 
 @bot.event
-async def on_member_ban(guild, user):
-    # 特定のチャンネルIDを設定
-    target_channel_id = 1126060676989849712
+async def on_member_ban(guild:discord.Guild, user:discord.User):
+    global target_channel_id
+    global guild_id
+    if guild.id != guild_id :
+        return
+    now = datetime.datetime.now()
+    target_channel = bot.get_channel(target_channel_id)
     banned_by = None
 
-    # 現在の日付と時間を取得
-    now = datetime.datetime.now()
-
-    # 特定のチャンネルにメッセージを送信
-    target_channel = bot.get_channel(target_channel_id)
-    if target_channel:
-        await target_channel.send(f'{now}')
-    
-    # BAN理由を取得
-    ban_entry = await guild.fetch_ban(user)
-    ban_reason = ban_entry.reason if ban_entry.reason else "理由は提供されていません。"
-    
-    # 特定のチャンネルにメッセージを送信
-    target_channel = bot.get_channel(target_channel_id)
     async for entry in guild.audit_logs(limit=1, action=discord.AuditLogAction.ban):
-        banned_by = entry.user
-        break
+        if entry.target == user:
+            banned_by = entry.user
+            break
     
     if target_channel:
-        await target_channel.send(f'{user.name} がBANされました。BANした人: {banned_by.name if banned_by else "不明"}')
-    
-    # BANされたユーザーのIDを取得
-    banned_user_id = user.id
-    
-    # 特定のチャンネルにメッセージを送信
-    if target_channel:
-        await target_channel.send(f'ユーザーID {banned_user_id} ')
-    # BAN理由を取得
-    ban_entry = await guild.fetch_ban(user)
-    ban_reason = ban_entry.reason if ban_entry.reason else "理由は提供されていません。"
-    
-    # 特定のチャンネルにメッセージを送信
-    target_channel = bot.get_channel(target_channel_id)
-    if target_channel:
-        await target_channel.send(f'理由: {ban_reason}')
+        embed = discord.Embed(
+            title=f"{banned_by.name}",
+            color=discord.Color.red()  # 赤色
+        )
+        
+        embed.timestamp=now
+        embed = Embed(
+            type="rich",
+            title="",
+            description=f"{user.mention} をBANしました",
+            color=0xff0000,
+            timestamp=now
+        )
+
+        embed.add_field(name="ID", value=user.name, inline=False)
+        embed.add_field(name="ニックネーム", value=user.global_name, inline=False)
+        if isinstance(user, discord.Member) and user.nick is not None:
+            embed.add_field(name="サーバーニックネーム", value=user.nick, inline=False)
+        ban_entry = await guild.fetch_ban(user)
+        ban_reason = ban_entry.reason if ban_entry.reason else '不明'
+        embed.add_field(name='BAN理由', value=ban_reason, inline=False)
+
+        embed.set_thumbnail(url=user.display_avatar.url)
+        embed.set_author(name="Member Banned", icon_url=user.display_avatar.url)
+        embed.set_footer(text=banned_by.name, icon_url=banned_by.display_avatar.url)
+        await target_channel.send(embed=embed)
+
+
+
+
 
 
 token = os.environ["DISCORD_TOKEN"]
